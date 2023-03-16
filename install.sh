4385ee0cf49d159db7811563cfd738051737e4cc
SalvaChLl <salvachll@gmail.com>
2023-03-16 23:56:49 +0100

    Configurar TMUX

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ chmod +x $BASEDIR/*
 
 $BASEDIR/main.sh $BASEDIR
 $BASEDIR/git.sh $BASEDIR
+$BASEDIR/nvm.sh $BASEDIR
+$BASEDIR/sym_links.sh $BASEDIR
+$BASEDIR/zsh.sh $BASEDIR
+$BASEDIR/vim.sh $BASEDIR
+$BASEDIR/tmux.sh $BASEDIR
--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+tmux source .config/tmux/tmux.conf
+
+echo Abre una sesión de TMUX y pulsa C-b-I
+read -p "Seguimos? " -n 1 -r
