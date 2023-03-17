f3871bbb73a1b1f7bea3185e1b8d939f33234de8
SalvaChLl <salvachll@gmail.com>
2023-03-17 14:20:40 +0100

    Descomentar git

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 git config --global user.name $USER
 git config --global user.email $USER@gmail.com
 git config --global user.mail $USER@gmail.com
 
-#ssh-keygen -t ed25519 -C "salvachll@gmail.com"
-#eval "$(ssh-agent -s)"
-#ssh-add ~/.ssh/id_ed25519
+ssh-keygen -t ed25519 -C "salvachll@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
 
 echo [GIT] -- COPIA LA SIGUIENTE CLAVE PÚBLICA EN TU CUENTA DE GITHUB
 cat ~/.ssh/id_ed25519.pub
