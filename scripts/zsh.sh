f5219007202a5d776716779c06a6f582e2d105f4
salvachll <salvachll@gmail.com>
2023-03-17 15:42:41 +0100

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -5,3 +5,5 @@ usermod --shell $(which zsh) $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 curl -L git.io/antigen > $HOME/.config/antigen.zsh
+
+cp -f ~/.zshrc.pre-oh-my-zsh ~/.zshrc
