0fb9f2b2c3629ca7af91529aada87cc7fc61d320
SalvaChLl <salvachll@gmail.com>
2023-03-21 13:44:53 +0100

    Add wakelock service

--- scripts/sym_links.sh
+++ scripts/sym_links.sh
@@ -5,3 +5,7 @@ ln -s -f ~/.config/nvim/init.vim ~/.vimrc
 ln -s -f ~/.config/tmux/tmux.conf ~/.tmux.conf
 
 chmod +x ~/.local/bin/monitor-switch
+sudo ln -s ~/.local/bin/monitor-switch /usr/bin/monitor-switch
+
+sudo cp wakelock@.service /etc/systemd/system/wakelock@.service
+sudo systemctl enable wakelock@$USER.service
